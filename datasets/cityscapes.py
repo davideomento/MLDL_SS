d90921d77a9ba888d1d44fb2d9fd6b9f18fc52c1
davideomento <s281464@studenti.polito.it>
2025-04-23 15:10:01 +0200

    first changes

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -1,21 +1,50 @@
+import torch
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.models as models
+from torchvision.transforms import functional as F
+from torch.utils.data import DataLoader
+from torch import nn
+from torchvision.models.segmentation import deeplabv3_resnet50
 from torch.utils.data import Dataset
+from PIL import Image
+import os
 
-# TODO: implement here your custom dataset class for Cityscapes
 
-
+# =====================
+# Dataset Definition
+# =====================
 class CityScapes(Dataset):
-    def __init__(self):
-        super(CityScapes, self).__init__()
-        # TODO
+    def _init_(self, root_dir, split='train', transform=None, target_transform=None):
+        self.root_dir = root_dir
+        self.split = split
+        self.transform = transform
+        self.target_transform = target_transform
 
-        pass
+        self.image_dir = os.path.join(root_dir, 'leftImg8bit', split)
+        self.label_dir = os.path.join(root_dir, 'gtFine', split)
 
-    def __getitem__(self, idx):
-        # TODO
+        self.image_paths = []
+        self.label_paths = []
 
-        pass
+        for city in os.listdir(self.image_dir):
+            city_path = os.path.join(self.image_dir, city)
+            for file_name in os.listdir(city_path):
+                if file_name.endswith('_leftImg8bit.png'):
+                    self.image_paths.append(os.path.join(city_path, file_name))
+                    label_file = file_name.replace('_leftImg8bit.png', '_gtFine_labelIds.png')
+                    self.label_paths.append(os.path.join(self.label_dir, city, label_file))
 
-    def __len__(self):
-        # TODO
+    def _len_(self):
+        return len(self.image_paths)
 
-        pass
+    def _getitem_(self, idx):
+        img = Image.open(self.image_paths[idx]).convert("RGB")
+        label = Image.open(self.label_paths[idx])
+
+        if self.transform:
+            img = self.transform(img)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return img, label
