2b321a1439f0c8bf4950e5176f28798103426211
Emanuelaa9 <egirlando53@gmail.com>
2025-05-06 10:52:49 +0000

    commit

--- train.py
+++ train.py
@@ -80,8 +80,8 @@ val_dataset = CityScapes(
     target_transform=LabelTransform()
 )
 
-train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=2)
-val_dataloader = DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=2)
+train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=6)
+val_dataloader = DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=6)
 
 # =====================
 # Model, Loss, Optimizer
