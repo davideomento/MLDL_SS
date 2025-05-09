0ad9dd11137c3b1dc1b604311756c89daa2d4b20
davideomento <s281464@studenti.polito.it>
2025-05-09 16:04:08 +0200

    u

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -3,13 +3,12 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 import numpy as np
-
 class CityScapes(torch.utils.data.Dataset):
     def __init__(self, image_dir, mask_dir, transform=None):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.transform = transform
-        self.images = sorted([os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.png')])  # Assicurati che questo percorso sia corretto
+        self.images = sorted([os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.png')])
         self.masks = sorted([os.path.join(mask_dir, f) for f in os.listdir(mask_dir) if f.endswith('.png')])
 
     def __getitem__(self, index):
