d051f02a01689e5fdf24a45a1278dcd64d05a38e
davideomento <s281464@studenti.polito.it>
2025-05-09 18:12:37 +0200

    y

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -4,35 +4,42 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 
-
-class CityScapes(torch.utils.data.Dataset):
+class CityScapes(Dataset):
     def __init__(self, root_dir, split='train', transform=None, target_transform=None):
         self.root_dir = root_dir
-        self.split = split  # 'train', 'val', 'test'
+        self.split = split
         self.transform = transform
         self.target_transform = target_transform
 
-        # Imposta i percorsi per le immagini e le maschere in base alla variabile `split`
-        image_dir = os.path.join(root_dir, 'leftImg8bit', split)
-        mask_dir = os.path.join(root_dir, 'gtFine', split)
+        self.image_dir = os.path.join(root_dir, 'images', split)
+        self.label_dir = os.path.join(root_dir, 'gtFine', split)
 
-        # Carica le immagini e le maschere
-        self.images = sorted([os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.png')])
-        self.masks = sorted([os.path.join(mask_dir, f) for f in os.listdir(mask_dir) if f.endswith('.png')])
+        self.image_paths = []
+        self.label_paths = []
 
-    def __getitem__(self, index):
-        image = Image.open(self.images[index]).convert("RGB")
-        mask = Image.open(self.masks[index])
+        for city in os.listdir(self.image_dir):
+            city_path = os.path.join(self.image_dir, city)
+            for file_name in os.listdir(city_path):
+                if file_name.endswith('_leftImg8bit.png'):
+                    self.image_paths.append(os.path.join(city_path, file_name))
+                    label_file = file_name.replace('_leftImg8bit.png', '_gtFine_labelTrainIds.png')
+                    self.label_paths.append(os.path.join(self.label_dir, city, label_file))
 
-        if self.transform:
-            transformed = self.transform(image=np.array(image), mask=np.array(mask))
-            image = transformed["image"]
-            mask = transformed["mask"]
-
-        if self.target_transform:
-            mask = self.target_transform(mask)
-
-        return image, mask.long()
 
     def __len__(self):
-        return len(self.images)
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.image_paths[idx]).convert("RGB")
+        label = Image.open(self.label_paths[idx])
+
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        if self.transform:
+            img = self.transform(img)
+
+
+        return img, label
+
+
