e2f44a2aa4c54efa492d779ab6251440234ed33e
davideomento <s281464@studenti.polito.it>
2025-05-29 16:28:20 +0200

    s

--- train_data_augmentation.py
+++ train_data_augmentation.py
@@ -85,9 +85,10 @@ img_transform_gta = A.Compose([
         A.HorizontalFlip(p=0.5),
         A.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1, p=0.5),
         A.GaussianBlur(blur_limit=(3, 3), sigma_limit=(0.1, 2.0), p=0.5),
-        #A.GaussNoise(var_limit=(10.0, 50.0)),
-        A.RandomFog(fog_coef_lower=0.1, fog_coef_upper=0.3, p=0.5),
-
+        A.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
+        #A.RandomFog(fog_coef_lower=0.1, fog_coef_upper=0.3, p=0.5),
+        A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),  # Low intensity
+        A.HueSaturationValue(hue_shift_limit=5, sat_shift_limit=10, val_shift_limit=10, p=0.5),  # Subtle color variation
         A.Normalize(mean=[0.485, 0.456, 0.406], 
                     std=[0.229, 0.224, 0.225]),
         ToTensorV2()
@@ -336,12 +337,12 @@ def validate(model, val_loader, criterion, epoch, num_classes=19):
 
 # Modificare la funzione main per raccogliere e salvare i dati
 def main():
-    checkpoint_path = os.path.join(save_dir, 'checkpoint_bisenet_aug.pth')
+    checkpoint_path = os.path.join(save_dir, 'checkpoint_bisenet_aug1.pth')
     var_model = "bisenet" 
     best_miou = 0
     start_epoch = 1
     init_lr = 2.5e-2
-    project_name = f"{var_model}_3b_jitter_fog_blur"
+    project_name = f"{var_model}_3b_jitter_bright_saturation_noise_blur"
 
     if os.path.exists(checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
