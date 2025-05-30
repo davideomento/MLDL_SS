898a5291b816a5e0cc7fa896413d19afe43f9409
davideomento <s281464@studenti.polito.it>
2025-05-30 09:20:35 +0200

    prova 16 batch siuze

--- train_data_augmentation.py
+++ train_data_augmentation.py
@@ -132,8 +132,8 @@ val_dataset = CityScapes_aug(
 print(f"Train dataset size: {len(train_dataset)}")
 print(f"Validation dataset size: {len(val_dataset)}")
 
-train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=2)
-val_dataloader = DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=2)
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=2)
+val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=2)
 
 # =====================
 # Model Setup
