2914fc2360538641889758a52b1671a6ebc5e276
davideomento <s281464@studenti.polito.it>
2025-06-04 23:23:13 +0200

    s

--- metrics.py
+++ metrics.py
@@ -9,6 +9,8 @@ from models.deeplabv2.deeplabv2 import get_deeplab_v2
 import os
 from models.bisenet.build_bisenet import BiSeNet
 import wandb
+from fvcore.nn import FlopCountAnalysis
+
 
 # ================================
 # Ambiente (Colab)
